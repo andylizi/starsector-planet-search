0464478ec6886455552331d9325fdf86d0cdecad
andylizi <andylizi666@gmail.com>
2023-05-14 15:46:24 +0800

    Relicense to MPL-2.0

--- src/main/java/net/andylizi/starsector/planetsearch/access/TextFieldAccess.java
+++ src/main/java/net/andylizi/starsector/planetsearch/access/TextFieldAccess.java
@@ -1,6 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
 package net.andylizi.starsector.planetsearch.access;
 
-import com.fs.starfarer.api.ui.LabelAPI;
 import com.fs.starfarer.api.ui.TextFieldAPI;
 import org.jetbrains.annotations.Nullable;
 
